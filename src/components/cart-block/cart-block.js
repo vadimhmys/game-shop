d4f0becd5828edab344471c607b53f51ef6d629b
Vadim Hmys <vadimhmyz@gmail.com>
2024-04-15 21:49:41 +0300

    feat: show and hide list of games in cart by click cart icon

--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { CartMenu } from "../cart-menu";
 import { BiCartAlt } from "react-icons/bi";
@@ -7,13 +7,20 @@ import { calcTotalPrice } from "../utils";
 
 
 export const CartBlock = () => {
+  const [isCartMenuVisible, setIsCartMenuVisible] = useState(false);
   const items = useSelector((state) => state.cart.itemsInCart);
   const totalPrice = calcTotalPrice(items);
   return (
     <div className="cart-block">
-      <BiCartAlt size={25} className="cart-block__icon"/>
-      { totalPrice > 0 && <span className="cart-block__total-price">{ totalPrice } руб.</span> }
-      <CartMenu items={items} onClick={() => null}/>
+      <BiCartAlt
+        size={25}
+        className="cart-block__icon"
+        onClick={() => setIsCartMenuVisible(!isCartMenuVisible)}
+      />
+      {totalPrice > 0 && (
+        <span className="cart-block__total-price">{totalPrice} руб.</span>
+      )}
+      {isCartMenuVisible && <CartMenu items={items} onClick={() => null} />}
     </div>
   );
 };
