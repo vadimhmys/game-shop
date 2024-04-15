2f2ef470a9c6a1dd55fd4f1e739f87b54d9e630f
Vadim Hmys <vadimhmyz@gmail.com>
2024-04-15 21:15:58 +0300

    feat: implement logic of adding and removing games from cart

--- src/components/game-buy/game-buy.js
+++ src/components/game-buy/game-buy.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { setItemInCart } from "../../redux/cart/reducer";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteItemFromCart, setItemInCart } from "../../redux/cart/reducer";
 import { Button } from "../button";
 import "./game-buy.css";
 
 export const GameBuy = ({game}) => {
   const dispatch = useDispatch();
+  const items = useSelector((state) => state.cart.itemsInCart);
+  let isItemInCart = items.some((item) => item.id === game.id );
   const handleClick = (e) => {
     e.stopPropagation();
-    dispatch(setItemInCart(game));
+    if (isItemInCart) {
+      dispatch(deleteItemFromCart(game.id));
+    } else {
+      dispatch(setItemInCart(game));
+    }
   };
   return (
     <div className="game-buy">
       <span className="game-buy__price">{game.price} руб.</span>
       <Button
-        type='primary'
+        type={isItemInCart ? "secondary" : 'primary' }
         onClick={ handleClick }>
-        В корзину
+          {isItemInCart ? "Убрать из корзины" : 'В корзину' }
       </Button>
     </div>
   );
