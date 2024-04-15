455579d4a594cff558a09062f3307ae896943212
Vadim Hmys <vadimhmyz@gmail.com>
2024-04-15 18:58:19 +0300

    feat: create store

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import {
   BrowserRouter as Router,
   Routes,
   Route
-} from 'react-router-dom'
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { HomePage } from './pages/home-page';
 import { Header } from './components/header';
+import { store } from './redux';
 
 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header/>
-        <Routes>
-          <Route path='/' element={<HomePage/>} />
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
