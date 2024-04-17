fc58207e95ab6ba469bc383f5b99dd2522e57c7c
Vadim Hmys <vadimhmyz@gmail.com>
2024-04-17 22:59:51 +0300

    feat: implement removing goods from cart

--- src/components/order-item/order-item.js
+++ src/components/order-item/order-item.js
@@ -1,10 +1,15 @@
 import React from "react";
-//import { useDispatch } from "react-redux";
-import "./order-item.css";
+import { useDispatch } from "react-redux";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { deleteItemFromCart } from "../../redux/cart/reducer";
 import { GameCover } from "../game-cover";
+import "./order-item.css";
 
 export const OrderItem = ({game}) => {
-  //const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(deleteItemFromCart(game.id));
+  };
   return (
     <div className="order-item">
       <div className="order-item__cover">
@@ -15,6 +20,11 @@ export const OrderItem = ({game}) => {
       </div>
       <div className="order-item__price">
         <span>{game.price} руб.</span>
+        <AiOutlineCloseCircle
+          size={25}
+          className="cart-item__delete-icon"
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
